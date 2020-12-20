587da72cc1cebced3c80dffb2cd9a0974e7be8d8
chris covarrubias <chris.covarrubias40@gmail.com>
2020-12-19 18:06:28 -0800

    employee-dir

--- employee-dir/src/App.js
+++ employee-dir/src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import FriendCard from './components/FriendCard';
+import EmployeeCard from './components/EmployeeCard';
 import Wrapper from './components/Wrapper';
 import Title from './components/Title';
-import friends from './friends.json';
+import employee from './friends.json';
 
 function App() {
   return (
     <Wrapper>
       <Title>Employee List</Title>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
+      <EmployeeCard
+        name={employee[0].name}
+        image={employee[0].image}
+        occupation={employee[0].occupation}
+        location={employee[0].location}
       />
       <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
+        name={employee[1].name}
+        image={employee[1].image}
+        occupation={employee[1].occupation}
+        location={employee[1].location}
       />
       <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
+        name={employee[2].name}
+        image={employee[2].image}
+        occupation={employee[2].occupation}
+        location={employee[2].location}
       />
     </Wrapper>
   );
